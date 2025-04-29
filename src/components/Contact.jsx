ac45597306dbcf85fc854a3211e2735108cb2f4c
Vinny00101 <vini123gtbr@gmail.com>
2025-04-29 20:23:13 -0300

    ADD: form for contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+
+export default function ContactComponent(){
+    return(
+        <>
+            <div className="conatiner-form">
+                <form className="form-contact" action="https://formsubmit.co/vini123gtbr@gmail.com" method="post">
+                    <input type="text" name="name" placeholder="Seu nome" required/>
+                    <input type="email" name="email" placeholder="E-mail" required/>
+                    <textarea name="message"  placeholder="Sua mensagem" required></textarea>
+                    <button type="submit">ENVIAR</button>
+                </form>
+            </div>
+        </>
+    )
+}
